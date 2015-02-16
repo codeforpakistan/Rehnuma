e5b554a5dccda7d643c63f107d8332af7857d07d
EbadUllah <ebad.flyingofficer@gmail.com>
2015-02-16 22:23:36 +0500

    Searchify Added

--- config/initializers/tanker.rb
+++ config/initializers/tanker.rb
@@ -1,3 +1,3 @@
 #Honoluluanswers::Application.config.index_tank_url = ENV['SEARCHIFY_API_URL']
-Honoluluanswers::Application.config.index_tank_url = ' http://:EQO40hX4cD4UMf@d5621.api.searchify.com'
+Honoluluanswers::Application.config.index_tank_url = 'http://:jIR5fE8HgLv5hp@72df.api.searchify.com'
 Honoluluanswers::Application.config.tanker_pagination_backend = :kaminari
